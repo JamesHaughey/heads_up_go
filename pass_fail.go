dd8e29e66a4668f1893445c8fd495f3bf73af6a2
James Haughey <jimmyhaugh@googlemail.com>
2020-02-09 18:53:54 +0000

    finish pass_fail

--- pass_fail.go
+++ pass_fail.go
@@ -5,11 +5,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"log"
+	"strings"
+	"strconv"
 )
 
 func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
-	input := reader.ReadString('\n')
-	fmt.Println(input)
+	input,err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("A grade of", grade, "is", status)
 }
